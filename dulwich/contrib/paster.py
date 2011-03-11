e3f4151bd6c4929e7b6fd9a626e03e40197904dc
David Blewett <davidb@sixfeetup.com>
2011-03-11 00:49:12 -0500

    Moved initial paster app_factory to dulwich.contrib.paster. Added 2 new filter_factories: make_gzip_filter and make_limit_input_filter. Updated sample paster config in docs/paster.ini to use new filters.

---
+++ dulwich/contrib/paster.py
@@ -0,0 +1,113 @@
+import os
+
+from dulwich import log_utils
+from dulwich.errors import NotGitRepository
+from dulwich.repo import Repo
+from dulwich.server import DictBackend
+from dulwich.web import (
+    GunzipFilter,
+    HTTPGitApplication,
+    LimitedInputFilter,
+)
+
+def make_app(global_config, **local_conf):
+    """Factory function for a Paster WSGI app
+    append_git=True will make each served git repo have .git appended to its
+        served URL.
+
+    Two options to serve: serve_dirs and individual URL path to operating
+    system path mappings.
+    Example:
+        File-system layout:
+            +-/var/lib/git
+            |-foo
+            |-bar
+            `-baz
+
+            +-/home/git
+            |-bing
+            `-bang
+
+        paster.ini:
+            [app:main]
+            use = egg:dulwich
+            append_git = True
+            serve_dirs =
+                /var/lib/git
+                /home/git
+            blerg = /home/dannyboy/src/blerg
+
+    Will result in the following being served:
+    /foo.git   => /var/lib/git/foo
+    /bar.git   => /var/lib/git/bar
+    /baz.git   => /var/lib/git/baz
+    /bing.git  => /home/git/bing
+    /bang.git  => /home/git/bang
+    /blerg.git => /home/dannyboy/src/blerg
+
+    NOTE: The last name definition wins. Whatever directory in serve_dirs is
+          last, or the last explicit mapping for the same name is what will
+          be mapped.
+    """
+    from paste.deploy.converters import asbool
+    from paste.deploy.converters import aslist
+    repos = {}
+    append_git = asbool(local_conf.pop('append_git', False))
+    serve_dirs = aslist(local_conf.pop('serve_dir', None))
+    log_utils.default_logging_config()
+
+    def add_repo(mapping, path, gitdir):
+        try:
+            mapping[path] = Repo(gitdir)
+        except NotGitRepository:
+            logger.error('Not a git repository, cannot serve: "%s".',
+                         gitdir)
+
+    if not local_conf:
+        add_repo(repos, '/', os.getcwd())
+    else:
+        if serve_dirs:
+            for top_dir in serve_dirs:
+                if not os.path.isdir(top_dir):
+                    logger.error('Not a directory, cannot serve: "%s".',
+                                 top_dir)
+
+                for d in os.listdir(top_dir):
+                    repo_path = '/'.join(('', d))
+                    gitdir = os.path.sep.join(top_dir, d)
+                    add_repo(repos, repo_path, gitdir)
+
+        for repo_name, gitdir in local_conf.items():
+            repo_path = '/'.join(('', repo_name))
+            add_repo(repos, repo_path, gitdir)
+
+    if not repos:
+        msg = 'No repositories to serve, check the ini file: "%s".'
+        logger.error(msg, global_config['__file__'])
+        raise IndexError(msg % global_config['__file__'])
+
+    if append_git:
+        new_repos = {}
+        for rpath, repo in repos.items():
+            if rpath.endswith('.git'):
+                # Don't be redundant...
+                new_repos[rpath] = repo
+                logger.debug('Not renaming, already ends in .git: "%s".',
+                             rpath)
+            else:
+                new_repos['.'.join((rpath, 'git'))] = repo
+        backend = DictBackend(new_repos)
+    else:
+        backend = DictBackend(repos)
+    return HTTPGitApplication(backend)
+
+
+def make_gzip_filter(global_config):
+    """Factory function to wrap a given WSGI application in a GunzipFilter,
+    to transparently decode Content-Encoding: gzip requests.
+    """
+    return GunzipFilter
+
+
+def make_limit_input_filter(global_config):
+    return LimitedInputFilter
--- setup.py
+++ setup.py
@@ -91,7 +91,11 @@ setup(name='dulwich',
           ],
       distclass=DulwichDistribution,
       entry_points={
-          'paste.app_factory': 'main=dulwich.web:make_paster_app'
+          'paste.app_factory': 'main=dulwich.contrib.paster:make_app',
+          'paste.filter_factory': [
+              'gzip=dulwich.contrib.paster:make_gzip_filter',
+              'limitinput=dulwich.contrib.paster:make_limit_input_filter',
+            ]
           },
       **setup_kwargs
       )
